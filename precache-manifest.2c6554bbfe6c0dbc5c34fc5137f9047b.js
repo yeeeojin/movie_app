e79565d83b5fae9502206bbc4b94ea065dcb8222
Yeo-Jin Hwang <itkings.yj@gmail.com>
2019-08-06 16:30:13 +0900

    Updates

---
+++ precache-manifest.2c6554bbfe6c0dbc5c34fc5137f9047b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c7c3ddee22fa991931fee57d14bb22",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "eec4d05346f04f811613",
+    "url": "/movie_app/static/css/main.6261e5c2.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/movie_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "eec4d05346f04f811613",
+    "url": "/movie_app/static/js/main.152f7d77.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
